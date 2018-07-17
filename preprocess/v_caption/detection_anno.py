3bda6541ccc3fdd324e2a58e4274f4f5ac5c4776
Unknown <sjson718@snu.ac.kr>
2018-07-17 15:49:21 +0900

    detection_anno.py add

---
+++ preprocess/v_caption/detection_anno.py
@@ -0,0 +1,238 @@
+import os
+import argparse
+import json
+import cv2
+import random
+import hgtk
+
+"""
+Class_Map
+0 : 가
+1 : 고 
+2 : 과
+3 : 각
+4 : 곡
+5 : 곽
+6 : ㄱ
+7 : a
+8 : 1
+9 : !
+"""
+
+CHO = (
+    u'ㄱ', u'ㄲ', u'ㄴ', u'ㄷ', u'ㄸ', u'ㄹ', u'ㅁ', u'ㅂ', u'ㅃ', u'ㅅ',
+    u'ㅆ', u'ㅇ', u'ㅈ', u'ㅉ', u'ㅊ', u'ㅋ', u'ㅌ', u'ㅍ', u'ㅎ'
+)
+
+JOONG = (
+    u'ㅏ', u'ㅐ', u'ㅑ', u'ㅒ', u'ㅓ', u'ㅔ', u'ㅕ', u'ㅖ', u'ㅣ',  # 길쭉이 0 ~ 8
+    u'ㅗ', u'ㅜ', u'ㅛ', u'ㅠ', u'ㅡ',                             # 넓쩍이 9 ~ 13
+    u'ㅘ', u'ㅙ', u'ㅚ', u'ㅝ', u'ㅞ', u'ㅟ', u'ㅢ'                # 길넓이 14 ~ 20
+)
+
+JONG = (
+    u'', u'ㄱ', u'ㄲ', u'ㄳ', u'ㄴ', u'ㄵ', u'ㄶ', u'ㄷ', u'ㄹ', u'ㄺ',
+    u'ㄻ', u'ㄼ', u'ㄽ', u'ㄾ', u'ㄿ', u'ㅀ', u'ㅁ', u'ㅂ', u'ㅄ', u'ㅅ',
+    u'ㅆ', u'ㅇ', u'ㅈ', u'ㅊ', u'ㅋ', u'ㅌ', u'ㅍ', u'ㅎ'
+)
+
+JAMO = CHO + JOONG + JONG[1:]
+FIRST_HANGUL_UNICODE = 0xAC00  # '가'
+LAST_HANGUL_UNICODE = 0xD7A3  # '힣'
+
+FIRST_NUMBER_UNICODE = 0x0030  # '0'
+LAST_NUMBER_UNICODE = 0x0039  # '9'
+
+FIRST_ALPHABET_UPPER_UNICODE = 0x0041  # 'A'
+LAST_ALPHABET_UPPER_UNICODE = 0x005A  # 'Z'
+
+FIRST_ALPHABET_LOWER_UNICODE = 0x0061  # 'a'
+LAST_ALPHABET_LOWER_UNICODE = 0x007A  # 'z'
+
+
+def is_jamo(letter):
+    return letter in JAMO
+
+def is_hangul(phrase): # TODO: need tuning!!
+    for letter in phrase:
+        code = ord(letter)
+        if (code < FIRST_HANGUL_UNICODE or code > LAST_HANGUL_UNICODE) and not is_jamo(letter):
+            return False
+
+    return True
+
+def is_alphabet(phrase): # TODO: need tuning!!
+    unicode_value = ord(phrase)
+    if (unicode_value >= FIRST_ALPHABET_UPPER_UNICODE and unicode_value <= LAST_ALPHABET_UPPER_UNICODE) \
+       or (unicode_value >= FIRST_ALPHABET_LOWER_UNICODE and unicode_value <= LAST_ALPHABET_LOWER_UNICODE):
+        return True
+    else:
+        return False
+
+def is_number(phrase): # TODO: need tuning!!
+    for letter in phrase:
+        code = ord(letter)
+        if (code < FIRST_NUMBER_UNICODE or code > LAST_NUMBER_UNICODE):
+            return False
+    return True
+
+def class_assign(caption):
+    try:
+        if len(caption) == 1:
+            if is_hangul(caption):
+
+                if is_jamo(caption):
+
+                    if caption in result_dict['hangul_jamo']:
+                        result_dict['hangul_jamo'][caption] += 1
+                    else:
+                        result_dict['hangul_jamo'][caption] = 1
+
+                    class_num = 6
+
+                else:
+
+                    if caption in result_dict['hangul']:
+                        result_dict['hangul'][caption] += 1
+                    else:
+                        result_dict['hangul'][caption] = 1
+
+                    eumso_list = hgtk.letter.decompose(caption)
+                    middle_sung = eumso_list[1]
+                    last_sung = eumso_list[2]
+
+                    if last_sung == JONG[0]:
+                        if middle_sung in JOONG[:9]:
+                            class_num = 0
+                        elif middle_sung in JOONG[9:14]:
+                            class_num = 1
+                        elif middle_sung in JOONG[14:]:
+                            class_num = 2
+                    elif last_sung in JONG[1:]:
+                        if middle_sung in JOONG[0:9]:
+                            class_num = 3
+                        elif middle_sung in JOONG[9:14]:
+                            class_num = 4
+                        elif middle_sung in JOONG[14:]:
+                            class_num = 5
+                    else:
+                        raise ValueError
+
+            elif is_alphabet(caption):
+
+                if caption in result_dict['alphabet']:
+                    result_dict['alphabet'][caption] += 1
+                else:
+                    result_dict['alphabet'][caption] = 1
+
+                class_num = 7
+
+            elif is_number(caption):
+
+                if caption in result_dict['number']:
+                    result_dict['number'][caption] += 1
+                else:
+                    result_dict['number'][caption] = 1
+
+                class_num = 8
+
+            else:
+                if caption in result_dict['etc']:
+                    result_dict['etc'][caption] += 1
+                else:
+                    result_dict['etc'][caption] = 1
+
+                class_num = 9
+
+        else:
+            if caption in result_dict['non_single']:
+                result_dict['non_single'][caption] += 1
+            else:
+                result_dict['non_single'][caption] = 1
+
+            if caption == '...':
+                class_num = 9
+            else:
+                class_num = 10000
+    except:
+        class_num = 10000
+
+    return class_num
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--anno_root", type=str, default="/media/son/Repository2/V.DO/V_Caption")
+    parser.add_argument("--valid_set", type=list, default=['000481', '000482', '001293', '001294', '001771', '001772'])
+    parser.add_argument("--task_name", type=str, default='ocr_demo2')
+
+    args = parser.parse_args()
+
+    anno_root = args.anno_root
+
+    with open(os.path.join(anno_root, '{}.json'.format(args.task_name))) as f:
+        anno = json.load(f)
+
+    f_1 = open('caption_train.txt', 'w')
+    f_2 = open('caption_val.txt', 'w')
+
+    result_dict = dict()
+    result_dict['hangul'] = dict()
+    result_dict['hangul_jamo'] = dict()
+    result_dict['number'] = dict()
+    result_dict['alphabet'] = dict()
+    result_dict['etc'] = dict()
+    result_dict['non_single'] = dict()
+
+    count = 0
+    for clip in anno['annotation']['clips']:
+
+        clip_name = clip['clip_name']
+
+        if clip_name in args.valid_set:
+            f = f_2  # validation set
+        else:
+            f = f_1  # training set
+
+        for image in clip['images']:
+
+            image_name = image['filename']
+            im = cv2.imread(os.path.join(anno_root, args.task_name, clip_name, image_name))
+            image_path = os.path.join(clip_name, image_name)
+            f.write(image_path)
+
+            for bbox in image['bbox']:
+
+                # class number assign with caption
+                caption = bbox['caption']
+                try:
+                    len(caption)
+                except:
+                    print('debug')
+                class_num = class_assign(caption)
+                if class_num == 10000:
+                    continue
+
+                x1 = bbox['start_x']
+                y1 = bbox['start_y']
+                x2 = bbox['end_x']
+                y2 = bbox['end_y']
+
+                bbox_xyxy = [x1, y1, x2, y2]
+                for coord in bbox_xyxy:
+                    coord = str(int(coord))
+
+                    f.write(' ' + coord)
+
+                f.write(' {}'.format(class_num))
+                print('Caption : {} | class_num : {}'.format(caption, class_num))
+
+            f.write('\n')
+            count += 1
+
+            print('{} processed'.format(count))
+
+    f_1.close()
+    f_2.close()
+    print('done')
